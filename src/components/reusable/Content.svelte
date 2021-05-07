b249f269ebbda8ff3d6a55cedde46ba6a8c8d207
Divin Irakiza <irakizadivin@gmail.com>
2021-05-07 10:34:50 +0200

    Tab Initiation

--- src/components/reusable/Content.svelte
+++ src/components/reusable/Content.svelte
@@ -5,13 +5,15 @@
 
 <div class="content-area">
     <div class="tabs-area">
-        <div class="tab">
+        <div class="tab active">
             <a href="#cards">Cards</a>
         </div>
         <div class="tab">
             <a href="#transactions">Transactions</a>
         </div>
     </div>
+
+    
 </div>
 
 <style>
@@ -19,6 +21,25 @@
     .tabs-area {
         border-bottom: 1px solid #E7E7E7;
         display: flex;
+    }
+
+    .tabs-area .tab {
+        padding: 0.5rem 0;
+        margin-right: 4rem;
+    }
+
+    .tabs-area .tab.active:before {
+        width: 6rem;
+        position: absolute;
+        bottom: -4px;
+        content: '';
+        height: 0.2rem;
+        background-color: #3648A5;
+    }
+
+    .tabs-area .tab a {
+        color: #2C405A;
+        font-size: 14px;
     }
 
 </style>
