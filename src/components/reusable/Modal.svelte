1e001f4c7587fc9cf4bbf4c7bfb0c9bae921bb1b
Divin Irakiza <irakizadivin@gmail.com>
2021-05-07 13:44:56 +0200

    Modal Popup Initiation

--- src/components/reusable/Modal.svelte
+++
@@ -1,7 +0,0 @@
-<div id="modal">
-  <div class="modal__window">
-      <a class="modal__close" href="#">X</a>
-      <h2>Please to meet you!</h2>
-      <p>Hello there, I am a nice Modal Window.</p> 
-  </div>
-</div>
