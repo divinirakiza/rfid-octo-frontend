2d268162c0969b0ec8b89dc8830fb650bfb201af
Divin Irakiza <irakizadivin@gmail.com>
2021-05-07 09:13:35 +0200

    Font families inclusion

--- src/components/reusable/Navbar.svelte
+++ src/components/reusable/Navbar.svelte
@@ -12,9 +12,15 @@
     @import url('https://fonts.googleapis.com/css2?family=Righteous&display=swap');
 
     .navbar {
-        font-family: 'Righteous', cursive;
         background-color: #190933;
         color: #fff;
+        padding: 1rem 2rem;
+    }
+
+    .navbar h3 {
+        font-size: 14px;
+        font-family: 'Noto Sans JP', sans-serif;
+        cursor: pointer;
     }
 
 </style>
---
+++ src/layouts/Layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+import Navbar from "../components/reusable/Navbar.svelte";
+
+	export let name: string;
+</script>
+
+<main>
+	<div class="navbar-area">
+		<Navbar />
+	</div>
+</main>
+
+<style>
+</style>
