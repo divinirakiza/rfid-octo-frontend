9d856cd66b86e192b132c28af9554e9306024e12
Divin Irakiza <irakizadivin@gmail.com>
2021-05-07 08:57:16 +0200

    Importing righteous font

---
+++ src/components/reusable/Navbar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<div class="navbar">
+    <div class="title">
+        <h3>RFID OCTO</h3>
+    </div>
+</div>
+
+<style>
+    
+
+</style>
