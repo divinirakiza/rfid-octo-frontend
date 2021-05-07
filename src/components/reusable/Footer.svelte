70852c7e3391f63194852ab24cb1bc1f8ce09578
Divin Irakiza <irakizadivin@gmail.com>
2021-05-07 10:01:25 +0200

    Added Footer

---
+++ src/components/reusable/Footer.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+</script>
+
+<div class="footer">
+    <div class="title">
+        <h3>RFID OCTO</h3>
+    </div>
+</div>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Righteous&display=swap');
+
+    .navbar {
+        background-color: #190933;
+        color: #fff;
+        padding: 1rem 2rem;
+    }
+
+    .navbar h3 {
+        font-size: 14px;
+        font-family: 'Noto Sans JP', sans-serif;
+        cursor: pointer;
+    }
+
+</style>
